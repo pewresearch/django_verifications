645ddc05f48555c31a466c81ebdc58ead59707f7
pewresearch <patrickvankessel@gmail.com>
2017-03-10 09:59:27 -0500

    cleaned up settings

--- django_verifications/settings.py
+++ django_verifications/settings.py
@@ -5,25 +5,19 @@ from django.conf import settings
 from django.db import models
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-# Copied from django_extensions as a boilerplate example
-# REPLACEMENTS = {
-# }
-# add_replacements = getattr(settings, 'EXTENSIONS_REPLACEMENTS', {})
-# REPLACEMENTS.update(add_replacements)
-
-from django.db import models
 
 DJANGO_VERIFICATIONS_FIELDS = ('fields_to_verify', 'verification_metadata_fields', 'verification_filters')
 models.options.DEFAULT_NAMES += DJANGO_VERIFICATIONS_FIELDS
 
-if not getattr(settings, 'DJANGO_VERIFICATIONS_BASE_MODEL', None):
-    # DJANGO_VERIFICATIONS_BASE_MODEL = models.Model
-    from pewtils.django.abstract_models import BasicExtendedModel
-    DJANGO_VERIFICATIONS_BASE_MODEL = BasicExtendedModel
-if not getattr(settings, 'DJANGO_VERIFICATIONS_MANAGER', None):
-    from pewtils.django.managers import BasicManager
-    DJANGO_VERIFICATIONS_BASE_MANAGER = BasicManager
-    #DJANGO_VERIFICATIONS_BASE_MANAGER = models.QuerySet
+from pewtils.django.managers import BasicManager
+from pewtils.django.abstract_models import BasicExtendedModel
+
+for setting, default in [
+    ("DJANGO_VERIFICATIONS_BASE_MODEL", BasicExtendedModel),
+    ("DJANGO_VERIFICATIONS_BASE_MANAGER", BasicManager)
+]:
+    if not getattr(settings, setting, None):
+        globals()[setting] = default
 
 TEMPLATES = [
     {
