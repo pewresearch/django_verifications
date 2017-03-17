71274f6d8aa81837b50ae61b6081313f42f8d158
pvankessel <pvankessel@pewresearch.org>
2017-03-17 09:26:20 -0400

    updated packaging info

--- django_verifications/settings.py
+++ django_verifications/settings.py
@@ -17,7 +17,7 @@ for setting, default in [
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
