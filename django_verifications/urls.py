d7c28a3dba7cae78f7c33ce6d30e18f9039e0cb9
pvankessel <pvankessel@pewresearch.org>
2017-03-31 16:59:32 -0400

    started work on a correction interface

--- django_verifications/urls.py
+++ django_verifications/urls.py
@@ -6,6 +6,8 @@ from django_verifications import views
 app_name = 'django_verifications'
 urlpatterns = [
     url(r'^$', views.home, name="home"),
-    url(r'^(?P<model_name>.+)/(?P<pk>[0-9]+)$', views.verify, name="verify"),
-    url(r'^(?P<model_name>.+)$', views.verify, name="verify"),
+    url(r'^verify/(?P<model_name>.+)/(?P<pk>[0-9]+)$', views.verify, name="verify"),
+    url(r'^verify/(?P<model_name>.+)$', views.verify, name="verify"),
+    url(r'^correct/(?P<model_name>.+)/(?P<pk>[0-9]+)$', views.correct, name="correct"),
+    url(r'^correct/(?P<model_name>.+)$', views.correct, name="correct"),
 ]
