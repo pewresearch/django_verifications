c0351d2b876c676376d1ad56628373cd1495d55c
pvankessel <pvankessel@pewresearch.org>
2017-03-31 15:21:36 -0400

    moved the version around again

--- django_verifications/__init__.py
+++ django_verifications/__init__.py
@@ -1,4 +1,4 @@
 import os
 
-with open(os.path.join(os.path.dirname(__file__), "VERSION"), "rb") as version_file:
+with open(os.path.join(os.path.dirname(__file__), "..", "VERSION"), "rb") as version_file:
     __version__ = version_file.read().strip()
