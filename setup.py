7b1c9970e6e1575641d03981f627d730d2b1eead
pvankessel <pvankessel@pewresearch.org>
2019-01-29 11:50:23 -0500

    python3 fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), "django_verifications", "VERSION"), "rb") as version_file:
-    __version__ = version_file.read().strip()
+    __version__ = str(version_file.read().strip())
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), "rb") as readme:
     README = readme.read()
