0576a601ecadb17e09efe1d76123ca973477e1db
pvankessel <pvankessel@pewresearch.org>
2019-09-26 14:12:46 -0400

    added unit tests and confirmed python 2/3 compatibility; also a few minor improvements

--- testapp/models.py
+++ testapp/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
-from django_queries.models import QueryModel
+from django_verifications.models import VerifiedModel
 
 
-class MovieReview(QueryModel):
+class MovieReview(VerifiedModel):
 
     text = models.TextField()
+
+    class Meta(object):
+        fields_to_verify = ["text"]
+        verification_filters = [{"text__regex": "action"}]
+
+    def get_verification_metadata(self):
+
+        return {"test": "test", "text": self.text}
--- testapp/tests/__init__.py
+++ testapp/tests/__init__.py
@@ -29,7 +29,68 @@ class BaseTests(DjangoTestCase):
 
     def test(self):
 
-        pass
+        from django_verifications.models import Verification
+        from django_verifications.exceptions import VerifiedFieldLock
+        from testapp.models import MovieReview
+        from django.contrib.auth import get_user_model
+
+        model_name = Verification.objects.available_model_names()[0]
+        model_name = model_name.replace(" ", "_")
+        self.assertEqual(model_name, "movie_review")
+
+        for query, value in [
+            ("flagged_for_verification", 5),
+            ("has_unexamined_fields", 5),
+            ("any_field_incorrect", 0),
+            ("all_fields_good_or_corrected", 0),
+        ]:
+            count = getattr(Verification.objects, query)(model_name).count()
+            self.assertEqual(count, value)
+            count = getattr(MovieReview.objects, query)().count()
+            self.assertEqual(count, value)
+
+        user = get_user_model().objects.create_user(
+            username="testuser", password="12345"
+        )
+
+        for obj in MovieReview.objects.flagged_for_verification():
+            for field in MovieReview._meta.fields_to_verify:
+                v = Verification.objects.create_or_update(
+                    {"user": user, "field": field, "content_object": obj},
+                    {"is_good": False, "corrected": False},
+                )
+
+        for query, value in [
+            ("flagged_for_verification", 5),
+            ("has_unexamined_fields", 0),
+            ("any_field_incorrect", 5),
+            ("all_fields_good_or_corrected", 0),
+        ]:
+            count = getattr(Verification.objects, query)(model_name).count()
+            self.assertEqual(count, value)
+            count = getattr(MovieReview.objects, query)().count()
+            self.assertEqual(count, value)
+
+        for obj in MovieReview.objects.flagged_for_verification():
+            for field in MovieReview._meta.fields_to_verify:
+                v = Verification.objects.create_or_update(
+                    {"user": user, "field": field, "content_object": obj},
+                    {"is_good": True, "corrected": True},
+                )
+
+        for query, value in [
+            ("flagged_for_verification", 5),
+            ("has_unexamined_fields", 0),
+            ("any_field_incorrect", 0),
+            ("all_fields_good_or_corrected", 5),
+        ]:
+            count = getattr(Verification.objects, query)(model_name).count()
+            self.assertEqual(count, value)
+            count = getattr(MovieReview.objects, query)().count()
+            self.assertEqual(count, value)
+
+        obj.text = "NEW TEXT"
+        self.assertRaises(VerifiedFieldLock, obj.save)
 
     def tearDown(self):
 
