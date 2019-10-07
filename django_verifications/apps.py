2501e85b391175b06950cba87f23620f2788f8d1
pvankessel <pvankessel@pewresearch.org>
2019-10-07 12:02:04 -0400

    switched settings over to app config

--- django_verifications/apps.py
+++ django_verifications/apps.py
@@ -11,8 +11,20 @@ class DjangoVerificationsConfig(AppConfig):
     def update_settings(self):
 
         from django.db import models
+        from django.conf import settings
 
         models.options.DEFAULT_NAMES += ("fields_to_verify", "verification_filters")
+        if "widget_tweaks" not in settings.INSTALLED_APPS:
+            settings.INSTALLED_APPS.append("widget_tweaks")
+
+        templates = settings.TEMPLATES
+        new_templates = []
+        for template in templates:
+            template["DIRS"].append(
+                os.path.join(DJANGO_VERIFICATIONS_BASE_DIR, "templates")
+            )
+            new_templates.append(template)
+        setattr(settings, "TEMPLATES", new_templates)
 
     def __init__(self, *args, **kwargs):
         super(DjangoVerificationsConfig, self).__init__(*args, **kwargs)
