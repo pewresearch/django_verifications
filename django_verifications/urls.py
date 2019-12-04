8b1cdb60dc27c55f6bdad406c3476411e9fdee8a
Brian Broderick <brian@broderick.io>
2019-12-04 17:53:51 +0000

    reject style receive bacon

--- django_verifications/urls.py
+++ django_verifications/urls.py
@@ -3,16 +3,12 @@ from django.conf.urls import url
 from django_verifications import views
 
 
-app_name = "django_verifications"
+app_name = 'django_verifications'
 urlpatterns = [
-    url(r"^$", views.home, name="home"),
-    url(r"^verify/(?P<model_name>.+)/(?P<pk>[0-9]+)$", views.verify, name="verify"),
-    url(r"^verify/(?P<model_name>.+)$", views.verify, name="verify"),
-    url(
-        r"^set_as_incorrect/(?P<model_name>.+)/(?P<pk>[0-9]+)/(?P<field>.+)$",
-        views.set_as_incorrect,
-        name="set_as_incorrect",
-    ),
-    url(r"^correct/(?P<model_name>.+)/(?P<pk>[0-9]+)$", views.correct, name="correct"),
-    url(r"^correct/(?P<model_name>.+)$", views.correct, name="correct"),
+    url(r'^$', views.home, name="home"),
+    url(r'^verify/(?P<model_name>.+)/(?P<pk>[0-9]+)$', views.verify, name="verify"),
+    url(r'^verify/(?P<model_name>.+)$', views.verify, name="verify"),
+    url(r'^set_as_incorrect/(?P<model_name>.+)/(?P<pk>[0-9]+)/(?P<field>.+)$', views.set_as_incorrect, name="set_as_incorrect"),
+    url(r'^correct/(?P<model_name>.+)/(?P<pk>[0-9]+)$', views.correct, name="correct"),
+    url(r'^correct/(?P<model_name>.+)$', views.correct, name="correct"),
 ]
