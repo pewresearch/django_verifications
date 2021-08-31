e5ce9242bc2b453123e9c2c4e0ab8bde2843b4d7
Brian Broderick <brian@broderick.io>
2021-08-31 11:36:33 +0000

    Bump version: 0.2.5.dev1 → 0.2.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt") as reqs:
 
 setup(
     name="django_verifications",
-    version='0.2.5.dev1',
+    version='0.2.5',
     description="Lightweight coding interface to manually review and freeze various model attributes using simple metadata toggles",
     long_description=readme,
     url="https://github.com/pewresearch/django_verifications",
